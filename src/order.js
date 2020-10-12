b10de8773101c1a85e2bcee9706f9642f0a63c45
Whitney Wilkes <wilkes.whit@gmail.com>
2020-10-11 21:02:43 -0600

    Add partial complete Order SRC

--- src/order.js
+++ src/order.js
@@ -1,7 +1,16 @@
+function takeOrder(order, deliveryOrders) {
+  if (deliveryOrders.length < 3) {
+    return deliveryOrders.push(order)
+  };
+};
+
+function refundOrder(orders, deliveryOrders) {
+
+};
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
+  takeOrder,
+  refundOrder,
   // listItems,
   // searchOrder
 }
